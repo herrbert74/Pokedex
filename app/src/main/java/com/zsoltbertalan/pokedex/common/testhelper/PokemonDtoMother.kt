0b7cfea680cdac36b5fe9b301253d467b81c6942
Zsolt Bertalan <herrbert74@gmail.com>
2024-07-07 12:58:12 +0100

    Fix inconsistency in TestMothers

--- app/src/main/java/com/zsoltbertalan/pokedex/common/testhelper/PokemonDtoMother.kt
+++ app/src/main/java/com/zsoltbertalan/pokedex/common/testhelper/PokemonDtoMother.kt
@@ -8,9 +8,11 @@ object PokemonDtoMother {
 		createDefaultPokemonDto(id = 0, name = "name1", hitPoints = 140, type = "Normal"),
 		createDefaultPokemonDto(id = 1, name = "name2", hitPoints = 30, type = "Grass"),
 		createDefaultPokemonDto(id = 2, name = "name3", hitPoints = 45, type = "Water"),
-		createDefaultPokemonDto(id = 3, name = "name4", hitPoints = 120, type = "Fire"),
+		createDefaultPokemonDto(id = 3, name = "name4", hitPoints = 120, type = "Fire", location = ""),
 		createDefaultPokemonDto(id = 4, name = "name5", hitPoints = 87, type = "Psychic"),
 		createDefaultPokemonDto(id = 5, name = "name6", hitPoints = 100, type = "Fighting"),
+		createDefaultPokemonDto(id = 6, name = "name7", hitPoints = 120, type = "Fire"),
+		createDefaultPokemonDto(id = 7, name = "name8", hitPoints = 120, type = "Fire", location = ""),
 	)
 
 }
@@ -21,7 +23,7 @@ private fun createDefaultPokemonDto(
 	evolutions: List<String> = emptyList(),
 	hitPoints: Int = 0,
 	imageUrl: String = "",
-	location: String = "",
+	location: String = "Kanto region",
 	name: String = "",
 	type: String = ""
 ): PokemonDto = PokemonDto(abilities, evolutions, hitPoints, id, imageUrl, location, name, type)
